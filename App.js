38779d6216e5b183c0e0e661f68b5a7b9e0b592c
Idraki Muhamad <idrakimuhamad@gmail.com>
2023-07-28 13:15:05 +0800

    init

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
+import 'expo-dev-client';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import FetchBlob from 'rn-fetch-blob';
 
 export default function App() {
+  const handle = () => {
+    FetchBlob.config({
+      trusty: true
+    })
+    .fetch('POST', 'replace-with-a-self-signed-url')
+    .then(res => res.json())
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
+  }
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button title='test' onPress={handle} />
     </View>
   );
 }
